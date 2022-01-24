0b777f57c0b23175d86934ce1993465794b012f8
SasiniD <sasidesilvaabc@gmail.com>
2022-01-24 13:09:34 +0530

    interpolation3 -configure interpolation delimitter

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  interpolation:['?','?'],
 })
 export class ProductsComponent implements OnInit {
 
