c515c3872b53639372ecb3f027320d98c9e7cddf
SasiniD <sasidesilvaabc@gmail.com>
2022-01-24 13:05:13 +0530

    interpolation 2 - usage of interpolation

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -11,5 +11,25 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  public firstProductItemName = "White Basmathi Rice" ;
+
+  public getPriceofRice(){
+    return 350;
+  }
+
+
+// variables related to show how private protected members work in interpolation.
+// Please do desired interpolation in html with following variables
+//private firstProductItemName = "White Basmathi Rice" ;
+//protected firstProductItemName = "White Basmathi Rice" ;
+  
+
+ 
+// variables related to show Invalid templates using Assignment, bitwise, increment and decrement perators (=,+=, -=, |, &, ++, –,!, ?. etc.)
+// Please do desired interpolation in html with following variables
+// public quantityOfRice : number = 5;
+//<td>{{quantityOfRice++}}</td>
+//<td>{{quantityOfRice + 1 }}</td>
 
 }
