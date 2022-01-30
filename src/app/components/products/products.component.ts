976a7f4ec08effb3e90654652a63fdcaa6cbcb74
SasiniD <sasidesilvaabc@gmail.com>
2022-01-31 03:29:58 +0530

    final output -bindings - removed  comments

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -29,20 +29,6 @@ export class ProductsComponent implements OnInit {
     return this.storage;
   }
 
-
-// variables related to show how private protected members work in interpolation.
-// Please do desired interpolation in html with following variables
-//private firstProductItemName = "White Basmathi Rice" ;
-//protected firstProductItemName = "White Basmathi Rice" ;
-  
-
- 
-// variables related to show Invalid templates using Assignment, bitwise, increment and decrement perators (=,+=, -=, |, &, ++, –,!, ?. etc.)
-// Please do desired interpolation in html with following variables
-// public quantityOfRice : number = 5;
-//<td>{{quantityOfRice++}}</td>
-//<td>{{quantityOfRice + 1 }}</td>
-
 public products = [{
   'productId' : "001",
   'productName' : "White Basmathi Rice",
