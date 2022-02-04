c302c607010408c5690408c08895bc5a548d4fab
SasiniD <sasidesilvaabc@gmail.com>
2022-02-04 17:43:10 +0530

    starting commit for services

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -10,32 +10,5 @@ export class AddProductComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    alert("ngOnInit Called");
-    console.log("triggered ngOnInit")
-  }
-
-  ngDoCheck() {
-    console.log("triggered ngDoCheck");
-  }
-
-  ngAfterContentInit() {
-    console.log("triggered ngAfterContentInit");
-  }
-
-  ngAfterContentChecked() {
-    console.log("triggered ngAfterContentChecked");
-  }
-
-  ngAfterViewInit() {
-    console.log("triggered ngAfterViewInit");
-  }
-
-  ngAfterViewChecked() {
-    console.log("triggered ngAfterViewChecked");
-  }
-
-  ngOnDestroy() {
-    alert("ngDestroy Called");
-    console.log("triggered ngOnDestroy");
   }
 }
